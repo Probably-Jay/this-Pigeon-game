c1366c037147eb6c1f312f892377755170e9e3dd
Jay Bretherton <jake.bretherton05@gmail.com>
2021-02-12 18:48:03 +0000

    Turn point progress

--- Assets/EventsManager.cs
+++ Assets/EventsManager.cs
@@ -13,27 +13,36 @@ public class EventsManager : Singleton<EventsManager>
 
     public enum EventType
     {
-
+          PlaceOwnObject = 0
+        , PlaceCompanionObject
+        , RemoveOwnObject
+        , WaterOwnPlant // perhaps rename to "maintain own object"
     }
 
-    Dictionary<EventType, Action<EventData>> events;
+
+
 
     public static new EventsManager Instance { get=> Singleton<EventsManager>.Instance; } //not needed but easier to find refs
 
     public override void Awake()
     {
         base.InitSingleton();
-        if(events!= null) Instance.events = new Dictionary<EventType, Action<EventData>>();
+        if(events != null) Instance.events = new Dictionary<EventType, Action>();
+        if(paramaterEvents!= null) Instance.paramaterEvents = new Dictionary<EventType, Action<EventParams>>();
     }
+
+
+    #region Non-Paramatized Events
+    Dictionary<EventType, Action> events;
 
     /// <summary>
     /// Add a new action to be triggered when an event is invoked 
     /// </summary>
     /// <param name="eventType">Event that will trigger the action</param>
     /// <param name="action">Delegate to the function that will be called when the action is triggered</param>
-    public static void BindEvent(EventType eventType, Action<EventData> action)
+    public static void BindEvent(EventType eventType, Action action)
     {
-        Action<EventData> newEvent;
+        Action newEvent;
         if (Instance.events.TryGetValue(eventType, out newEvent))
         {
             newEvent += action;
@@ -51,11 +60,12 @@ public class EventsManager : Singleton<EventsManager>
     /// </summary>
     /// <param name="eventType">Event that would have triggered the action</param>
     /// <param name="action">Delegate to the function that will no longer be called when the action is triggered</param>
-    public static void UnbindEvent(EventType eventType, Action<EventData> action)
+    public static void UnbindEvent(EventType eventType, Action action)
     {
+        Action thisEvent;
+        Dictionary<EventType, Action> events1 = Instance.events;
         if (!InstanceExists) { WarnInstanceDoesNotExist(); return; }
-        Action<EventData> thisEvent;
-        if (Instance.events.TryGetValue(eventType, out thisEvent))
+        if (events1.TryGetValue(eventType, out thisEvent))
         {
             thisEvent -= action;
             Instance.events[eventType] = thisEvent;
@@ -67,20 +77,76 @@ public class EventsManager : Singleton<EventsManager>
     /// Call every function listening for this event
     /// </summary>
     /// <param name="eventType">Type of event to trigger</param>
-    /// <param name="paramaters">Optional: Additional paramaters to be passed to the function</param>
-    public static void InvokeEvent(EventType eventType, EventData paramaters = default)
+    public static void InvokeEvent(EventType eventType)
     {
-        if (Instance.events.ContainsKey(eventType))
+        if (Instance.paramaterEvents.ContainsKey(eventType))
         {
-            Instance.events[eventType]?.Invoke(paramaters);
+            Instance.events[eventType]?.Invoke();
         }
         else Debug.LogError($"Invoke failed. Event {eventType.ToString()} is not a member of the events list");
     }
+    #endregion
 
 
-    public struct EventData
+    #region Paramatized Events
+    Dictionary<EventType, Action<EventParams>> paramaterEvents;
+
+    /// <summary>
+    /// Add a new action to be triggered when an event is invoked 
+    /// </summary>
+    /// <param name="eventType">Event that will trigger the action</param>
+    /// <param name="action">Delegate to the function that will be called when the action is triggered</param>
+    public static void BindEvent(EventType eventType, Action<EventParams> action)
+    {
+        Action<EventParams> newEvent;
+        if (Instance.paramaterEvents.TryGetValue(eventType, out newEvent))
+        {
+            newEvent += action;
+            Instance.paramaterEvents[eventType] = newEvent;
+        }
+        else
+        {
+            newEvent += action;
+            Instance.paramaterEvents.Add(eventType, newEvent);
+        }
+    }
+
+    /// <summary>
+    /// Remove action from listening for an event
+    /// </summary>
+    /// <param name="eventType">Event that would have triggered the action</param>
+    /// <param name="action">Delegate to the function that will no longer be called when the action is triggered</param>
+    public static void UnbindEvent(EventType eventType, Action<EventParams> action)
+    {
+        if (!InstanceExists) { WarnInstanceDoesNotExist(); return; }
+        Action<EventParams> thisEvent;
+        if (Instance.paramaterEvents.TryGetValue(eventType, out thisEvent))
+        {
+            thisEvent -= action;
+            Instance.paramaterEvents[eventType] = thisEvent;
+        }
+        else Debug.LogWarning($"Unsubscribe failed. Event {eventType.ToString()} is not a member of the events list");
+    }
+
+    /// <summary>
+    /// Call every function listening for this event
+    /// </summary>
+    /// <param name="eventType">Type of event to trigger</param>
+    /// <param name="paramaters">Optional: Additional paramaters to be passed to the function</param>
+    public static void InvokeEvent(EventType eventType, EventParams paramaters)
+    {
+        if (Instance.paramaterEvents.ContainsKey(eventType))
+        {
+            Instance.paramaterEvents[eventType]?.Invoke(paramaters);
+        }
+        else Debug.LogError($"Invoke failed. Event {eventType.ToString()} is not a member of the events list");
+    }
+    #endregion
+
+    public struct EventParams
     {
         public int IntData;
+        public Enum EnumData;
     }
 
 }
--- Assets/TurnPoints.cs
+++ Assets/TurnPoints.cs
@@ -4,39 +4,77 @@ using UnityEngine;
 
 public class TurnPoints : MonoBehaviour
 {
+
     [Header("Start each round with the following number of points")]
     [SerializeField] int placeOwnPlantPointsInitial;
     [SerializeField] int placeCompanionPlantPointsInitial;
     [SerializeField] int removeOwnPlantPointsInitial;
     [SerializeField] int waterOwnPlantPointsInitial;
 
-    public int OwnPlantPlacePoints { get; private set; }
-    public int CompanionPlacePoints { get; private set; }
-    public int OwnRemovePoints { get; private set; }
-    public int OwnWaterPoints { get; private set; }
-
-    private void OnEnable()
+    public enum PointType
     {
-        
+        OurObjectPlace
+        , CompanionPlace
+        , OurObjectRemove
+        , OurWater
+
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void StartTurn()
     {
-        OwnPlantPlacePoints = placeOwnPlantPointsInitial;
-        CompanionPlacePoints = placeCompanionPlantPointsInitial;
-        OwnRemovePoints = removeOwnPlantPointsInitial;
-        OwnWaterPoints = waterOwnPlantPointsInitial;
+        points[PointType.OurObjectPlace] = placeOwnPlantPointsInitial;
+        points[PointType.CompanionPlace] = placeCompanionPlantPointsInitial;
+        points[PointType.OurObjectRemove] = removeOwnPlantPointsInitial;
+        points[PointType.OurWater] = waterOwnPlantPointsInitial;
     }
+
+
+    Dictionary<PointType, int> points;
+
+
+
+    private void Start()
+    {
+        points = new Dictionary<PointType, int>();
+        foreach (PointType type in System.Enum.GetValues(typeof(PointType)))
+        {
+            points.Add(type, 0);
+        }
+    }
+
+
+    public int GetPoints(PointType type) => points[type];
+    private void SetPoints(PointType type, int value) => points[type] = value >= 0 ? value : 0;
+    private void DecreasePoints(PointType type) => SetPoints(type, GetPoints(type) - 1);
+
+
+    private void OnEnable()
+    {
+        EventsManager.BindEvent(EventsManager.EventType.PlaceOwnObject, DecreaseOurPlacePoints);
+        EventsManager.BindEvent(EventsManager.EventType.PlaceCompanionObject, DecreaseCompanionPlacePoints);
+        EventsManager.BindEvent(EventsManager.EventType.RemoveOwnObject, DecreaseOurRemovePoints);
+        EventsManager.BindEvent(EventsManager.EventType.WaterOwnPlant, DecreaseOurWaterPoints);
+    }
+
+    private void OnDisable()
+    {
+        EventsManager.UnbindEvent(EventsManager.EventType.PlaceOwnObject, DecreaseOurPlacePoints);
+        EventsManager.UnbindEvent(EventsManager.EventType.PlaceCompanionObject, DecreaseCompanionPlacePoints);
+        EventsManager.UnbindEvent(EventsManager.EventType.RemoveOwnObject, DecreaseOurRemovePoints);
+        EventsManager.UnbindEvent(EventsManager.EventType.WaterOwnPlant, DecreaseOurWaterPoints);
+    }
+
+
+
+    private void DecreaseOurPlacePoints() => DecreasePoints(PointType.OurObjectPlace);
+    private void DecreaseCompanionPlacePoints() => DecreasePoints(PointType.CompanionPlace);
+    private void DecreaseOurRemovePoints() => DecreasePoints(PointType.OurObjectRemove);
+    private void DecreaseOurWaterPoints() => DecreasePoints(PointType.OurWater);
+
+
+
+
 }
+
