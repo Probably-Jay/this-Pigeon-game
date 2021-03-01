20074a503a00e816244481a3ad7ee3f49f51a3b4
TheGamerXander <1703361@uad.ac.uk>
2021-03-01 21:11:40 +0100

    Grids now works on place holder background image

--- Assets/Scripts/Grid/GridControls.cs
+++ Assets/Scripts/Grid/GridControls.cs
@@ -9,36 +9,59 @@ public class GridControls : MonoBehaviour
 {
     // starting information for first grid
    //[SerializeField] 
-    Vector3 startingPositionGrid1 = new Vector3(-7.0f, 3.2f, 0.0f);
+    Vector3 startingPositionGrid1 = new Vector3(-1.98f, 2.2f, 0.0f);
     float tileSizeGrid1 = 1.0f;
-    int columnsGrid1 = 10;
-    int rowsGrid1 = 7;
+    int columnsGrid1 = 2;
+    int rowsGrid1 = 5;
 
-    Vector3 startingPositionGrid2 = new Vector3(-7.0f, 13.2f, 0.0f);
+    Vector3 startingPositionGrid2 = new Vector3(0.8f, 2.2f, 0.0f);
+    
     float tileSizeGrid2 = 1.0f;
-    int columnsGrid2 = 10;
-    int rowsGrid2 = 7;
+    int columnsGrid2 = 2;
+    int rowsGrid2 = 6;
 
-    [SerializeField] GameObject gridP1Gameobject;
-    [SerializeField] GameObject gridP2Gameobject;
+    Vector3 startingPositionGrid3 = new Vector3(1.0f, 12.2f, 0.0f);
+    float tileSizeGrid3 = 1.0f;
+    int columnsGrid3 = 2;
+    int rowsGrid3 = 5;
+
+    Vector3 startingPositionGrid4 = new Vector3(-1.8f, 12.0f, 0.0f);
+    float tileSizeGrid4 = 1.0f;
+    int columnsGrid4 = 2;
+    int rowsGrid4 = 6;
+
+   
+
+    [SerializeField] GameObject grid1P1Gameobject;
+    [SerializeField] GameObject grid2P1Gameobject;
+
+
+    [SerializeField] GameObject grid1P2Gameobject;
+    [SerializeField] GameObject grid2P2Gameobject;
 
     // Update is called once per frame
     void Awake()
     {
 
-        gridP1Gameobject = new GameObject("Grid Player one");
-        gridP2Gameobject = new GameObject("Grid Player one");
+        grid1P1Gameobject = new GameObject("Grid1 Player one");
+        grid2P1Gameobject = new GameObject("Grid2 Player one");
 
-        var gridP1 = gridP1Gameobject.AddComponent<TheGrid>();
-        var gridP2 = gridP2Gameobject.AddComponent<TheGrid>();
 
-        gridP1.Init(startingPositionGrid1, tileSizeGrid1, columnsGrid1, rowsGrid1);
-        gridP2.Init(startingPositionGrid2, tileSizeGrid2, columnsGrid2, rowsGrid2);
+        grid1P2Gameobject = new GameObject("Grid1 Player Two");
+        grid2P2Gameobject = new GameObject("Grid2 Player Two");
 
-            //// creates a new grid passing it the starting information
-            //TheGrid gridP1 = new TheGrid(startingPositionGrid1, tileSizeGrid1, columnsGrid1, rowsGrid1);
-       
-            //TheGrid gridP2 = new TheGrid(startingPositionGrid2, tileSizeGrid2, columnsGrid2, rowsGrid2);
+
+        var grid1P1 = grid1P1Gameobject.AddComponent<TheGrid>();
+        var grid2P1 = grid2P1Gameobject.AddComponent<TheGrid>();
+
+        var grid1P2 = grid1P2Gameobject.AddComponent<TheGrid>();
+        var grid2P2 = grid2P2Gameobject.AddComponent<TheGrid>();
+
+        grid1P1.Init(startingPositionGrid1, tileSizeGrid1, columnsGrid1, rowsGrid1);
+        grid2P1.Init(startingPositionGrid2, tileSizeGrid2, columnsGrid2, rowsGrid2);
+
+        grid1P2.Init(startingPositionGrid3, tileSizeGrid3, columnsGrid3, rowsGrid3);
+        grid2P2.Init(startingPositionGrid4, tileSizeGrid4, columnsGrid4, rowsGrid4);
 
     }
 }
