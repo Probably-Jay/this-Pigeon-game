4d2094a958ef2c278563f8bb27ca4774db29b184
Jay Bretherton <jake.bretherton05@gmail.com>
2021-03-28 15:36:53 +0100

    Auto stash before merge of "ZF-Branch" and "origin/ZF-Branch"

---
+++ Assets/Scripts/Game/Plants/PlantIconsDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Plants
+{
+    /// <summary>
+    /// Class that displays the icons that a plant needs
+    /// </summary>
+    [RequireComponent(typeof(Plant))]
+    [RequireComponent(typeof(PlantGrowth))]
+    public class PlantIconsDisplay : MonoBehaviour
+    {
+        PlantGrowth plantGrowth;
+        Plant palnt;
+        private void Awake()
+        {
+            plantGrowth = GetComponent<PlantGrowth>();
+        }
+
+        private void OnEnable()
+        {
+            plantGrowth.TendingState.OnPlantTended += TendingState_OnPlantTended;
+        }
+
+
+        private void OnDisable()
+        {
+            plantGrowth.TendingState.OnPlantTended -= TendingState_OnPlantTended;
+        }
+
+
+        private void TendingState_OnPlantTended()
+        {
+            var a = plantGrowth.acti
+        }
+    }
+}
