0900397be647659e4b5567ee101d722fe7ddfce8
Zapstereo <fernandezzap@gmail.com>
2021-03-28 15:42:16 +0100

    Seed Menu and Button scripts

---
+++ Assets/Scripts/UI/PlantButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantButton : MonoBehaviour
+{
+    public GameObject myPlant;
+    public CurrentSeedStorage seedStorage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void GoToPlanting()
+    {
+        seedStorage.SetCurrentPlant(myPlant);
+        EventsManager.InvokeEvent(EventsManager.EventType.PlantingBegin);
+    }
+}
