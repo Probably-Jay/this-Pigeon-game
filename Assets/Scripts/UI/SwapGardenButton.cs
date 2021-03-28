9ebe1c4a42d4f766a141fa3b847713e6db7ef150
Jay Bretherton <jake.bretherton05@gmail.com>
2021-03-28 20:09:33 +0100

    quit button

--- Assets/Scripts/UI/SwapGardenButton.cs
+++ Assets/Scripts/UI/SwapGardenButton.cs
@@ -3,38 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-[RequireComponent(typeof(Button),typeof(Animator))]
-public class SwapGardenButton : MonoBehaviour
+namespace SceneUI
 {
+    [RequireComponent(typeof(Button), typeof(Animator))]
+    public class SwapGardenButton : MonoBehaviour
+    {
 
-    Animator animator;
-    private void Awake()
-    {
-        animator = GetComponent<Animator>();
-    }
-    private void OnEnable()
-    {
-        EventsManager.BindEvent(EventsManager.ParameterEventType.SwappedGardenView, RotateButton);
-    }
-
-    private void OnDisable()
-    {
-        EventsManager.UnbindEvent(EventsManager.ParameterEventType.SwappedGardenView, RotateButton);
-    }
-
-    private void RotateButton(EventsManager.EventParams paramaters)
-    {
-        switch ((Player.PlayerEnum)paramaters.EnumData)
+        Animator animator;
+        private void Awake()
         {
-            case Player.PlayerEnum.Player1:
-                animator.SetTrigger("SwitchToPlayerOne");
-                break;
-            case Player.PlayerEnum.Player2:
-                animator.SetTrigger("SwitchToPlayerTwo");
-                break;
-            default:
-                break;
+            animator = GetComponent<Animator>();
+        }
+        private void OnEnable()
+        {
+            EventsManager.BindEvent(EventsManager.ParameterEventType.SwappedGardenView, RotateButton);
+        }
+
+        private void OnDisable()
+        {
+            EventsManager.UnbindEvent(EventsManager.ParameterEventType.SwappedGardenView, RotateButton);
+        }
+
+        private void RotateButton(EventsManager.EventParams paramaters)
+        {
+            switch ((Player.PlayerEnum)paramaters.EnumData)
+            {
+                case Player.PlayerEnum.Player1:
+                    animator.SetTrigger("SwitchToPlayerOne");
+                    break;
+                case Player.PlayerEnum.Player2:
+                    animator.SetTrigger("SwitchToPlayerTwo");
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
