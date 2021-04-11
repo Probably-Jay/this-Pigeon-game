1475ca85901ceb7a20c643e9c4f5bdc89185f2f4
Jay Bretherton <jake.bretherton05@gmail.com>
2021-04-12 00:34:31 +0100

    begin

--- Assets/Scripts/Net/Playfab/NetworkGame.cs
+++ Assets/Scripts/Net/Playfab/NetworkGame.cs
@@ -27,6 +27,7 @@ namespace NetSystem
             public bool Open;
             public PlayFab.CloudScriptModels.EntityKey Player1;
             public PlayFab.CloudScriptModels.EntityKey Player2;
+            public string SharedDataID;
         }
 
     }
