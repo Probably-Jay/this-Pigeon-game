c39b3749e20b1c4df4013bf1649a9e311630915b
TheGamerXander <1703361@uad.ac.uk>
2021-04-22 18:51:27 +0200

    Planting bug fixed

--- Assets/Scripts/UI/UICommands.cs
+++ Assets/Scripts/UI/UICommands.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 //created Zap 26/03
+// edited Xander 22/04/2021
 public class UICommands : MonoBehaviour
 {
     //[SerializeField] Button button;
@@ -25,21 +26,52 @@ public class UICommands : MonoBehaviour
 
     public void OpenSeedBag()
     {
-        bool canPlaceInOwnGarden = (GameManager.Instance.ActivePlayer.TurnPoints.HasPointsLeft(TurnPoints.PointType.SelfObjectPlace) && GameManager.Instance.InOwnGarden);
-        bool canPlaceInCompanionsGarden = (!GameManager.Instance.InOwnGarden && GameManager.Instance.ActivePlayer.TurnPoints.HasPointsLeft(TurnPoints.PointType.SelfObjectPlace));
+        // Xanders edit ~ fixes bug that allows you to plant unlimited plants in the other players garden if
+        // the active player has not placed their plant yet and not allow any planting in the other players garden if they have
+         
 
-        if (canPlaceInOwnGarden || canPlaceInCompanionsGarden)
+        // for planting in active players own garden 
+
+        if (GameManager.Instance.InOwnGarden && GameManager.Instance.ActivePlayer.TurnPoints.HasPointsLeft(TurnPoints.PointType.SelfObjectPlace))
         {
             EventsManager.InvokeEvent(EventsManager.EventType.SeedBagOpen);
         }
-        else if(GameManager.Instance.InOwnGarden)
+        else if (GameManager.Instance.InOwnGarden)
         {
             EventsManager.InvokeEvent(EventsManager.ParameterEventType.NotEnoughPointsForAction, new EventsManager.EventParams() { EnumData = TurnPoints.PointType.SelfObjectPlace });
         }
-        else if(!GameManager.Instance.InOwnGarden)
+
+
+        // for tha active player planting in the non-active players garden 
+        if (!GameManager.Instance.InOwnGarden && GameManager.Instance.ActivePlayer.TurnPoints.HasPointsLeft(TurnPoints.PointType.OtherObjectPlace))
+        {
+            EventsManager.InvokeEvent(EventsManager.EventType.SeedBagOpen);
+        }else if(!GameManager.Instance.InOwnGarden)
         {
             EventsManager.InvokeEvent(EventsManager.ParameterEventType.NotEnoughPointsForAction, new EventsManager.EventParams() { EnumData = TurnPoints.PointType.OtherObjectPlace });
         }
+        // end of Xanders edit
+
+
+
+        //bool canPlaceInOwnGarden = (GameManager.Instance.ActivePlayer.TurnPoints.HasPointsLeft(TurnPoints.PointType.SelfObjectPlace) && GameManager.Instance.InOwnGarden);
+        //bool canPlaceInCompanionsGarden = (!GameManager.Instance.InOwnGarden && GameManager.Instance.ActivePlayer.TurnPoints.HasPointsLeft(TurnPoints.PointType.SelfObjectPlace));
+
+
+        //if (canPlaceInOwnGarden || canPlaceInCompanionsGarden)
+        //{
+        //    EventsManager.InvokeEvent(EventsManager.EventType.SeedBagOpen);
+        //}
+        //else if(GameManager.Instance.InOwnGarden)
+        //{
+        //    EventsManager.InvokeEvent(EventsManager.ParameterEventType.NotEnoughPointsForAction, new EventsManager.EventParams() { EnumData = TurnPoints.PointType.SelfObjectPlace });
+        //}
+        //else if(!GameManager.Instance.InOwnGarden)
+        //{
+        //    EventsManager.InvokeEvent(EventsManager.ParameterEventType.NotEnoughPointsForAction, new EventsManager.EventParams() { EnumData = TurnPoints.PointType.OtherObjectPlace });
+        //}
+
+
     }
     public void CloseSeedBag() { EventsManager.InvokeEvent(EventsManager.EventType.SeedBagClose); Debug.Log("Closed Bag"); }
     public void OkayNext() => EventsManager.InvokeEvent(EventsManager.EventType.DialogueNext);
