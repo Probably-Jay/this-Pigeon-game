21f6e450fc06d3e7987fbac792d758ae4f2f37ea
TheXander <xander.purvis@gmail.com>
2021-04-27 02:44:57 +0200

    Update to data gathering

---
+++ Assets/Scripts/Data_Managment/DataManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    PlayerDataPacket playerData;
+    GardenDataPacket gardenData;
+
+
+    void Awake()
+    {
+        playerData = GetComponent<PlayerDataPacket>();
+        gardenData = GetComponent<GardenDataPacket>();
+    }
+
+
+   public void SetPlayer1Name(string newID)
+   {
+        playerData.player1ID = newID;
+   }
+
+   public void SetPlayer2Name(string newID)
+   {
+       playerData.player1ID = newID;
+   }
+
+   public void AddPlantToGarden1(
+    int plantType,
+    int slotNumber,
+    int stage,
+    bool action1,
+    bool action2,
+    bool action3,
+    bool action4)
+   {
+        gardenData.newestGarden1.Add(new GardenDataPacket.Plant(plantType, slotNumber, stage, action1, action2, action3, action4));
+   }
+
+   public void AddPlantToGarden2()
+   {
+
+   }
+
+}
---
+++ Assets/Scripts/Data_Managment/GardenDataPacket.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GardenDataPacket : MonoBehaviour
+{
+    public struct Plant
+    {
+        public int plantType;
+        public int slotNumber;
+        public int stage;
+
+        public bool action1;
+        public bool action2;
+        public bool action3;
+        public bool action4;
+
+        public Plant(int PlantType,
+                     int SlotNumber,
+                     int Stage,
+                     bool Action1,
+                     bool Action2,
+                     bool Action3,
+                     bool Action4)
+        {
+
+            plantType = PlantType;
+            slotNumber = SlotNumber;
+            stage = Stage;
+
+            action1 = Action1;
+            action2 = Action2;
+            action3 = Action3;
+            action4 = Action4;
+        }
+      
+    }
+
+   public List<Plant> newestGarden1;
+   public List<Plant> newestGarden2;
+
+   public List<Plant> oldGarden1;
+   public List<Plant> oldGarden2;
+}
