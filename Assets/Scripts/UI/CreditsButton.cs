2b2f2cf4301964a8babead241d4e462badc1b070
Safya D <31994731+SafyaDev@users.noreply.github.com>
2021-04-28 18:05:31 +0200

    Changed lay out settings menu and added credits button

---
+++ Assets/Scripts/UI/CreditsButton.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MonoBehaviour
+{
+    //public void QuitToMenu() => SceneChangeController.Instance.ChangeScene(SceneChangeController.Scenes.Credits);
+
+}
