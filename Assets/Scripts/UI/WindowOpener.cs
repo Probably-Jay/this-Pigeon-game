b63e8adc1469e327bbd931d679fefe016e8d4148
Scert <scottafjarvis@gmail.com>
2021-04-29 22:26:51 +0100

    Revert "Merge branch 'dev_main' into SJ-MoreSounds"
    This reverts commit 2b8d278aacd75b3cbbb5ced960b385b368bab938, reversing
    changes made to 444d9a50e208edf977bced11e42887ee5a7c0e6c.

--- Assets/Scripts/UI/WindowOpener.cs
+++ Assets/Scripts/UI/WindowOpener.cs
@@ -7,16 +7,13 @@ using UnityEngine.UI;
 public class WindowOpener : MonoBehaviour
 {
     public GameObject seedBag;
-    public GameObject trowelConfirm;
     // Start is called before the first frame update
     void OnEnable()
     {
-        EventsManager.BindEvent(EventsManager.EventType.triedToRemoveOwnObject, OpenTrowelConfirm);
         EventsManager.BindEvent(EventsManager.EventType.SeedBagOpen, OpenSeedBag);
     }
     private void OnDisable()
     {
-        EventsManager.UnbindEvent(EventsManager.EventType.triedToRemoveOwnObject, OpenTrowelConfirm);
         EventsManager.UnbindEvent(EventsManager.EventType.SeedBagOpen, OpenSeedBag);
     }
 
@@ -24,9 +21,5 @@ public class WindowOpener : MonoBehaviour
     void OpenSeedBag()
     {
         seedBag.SetActive(true);
-    }
-    void OpenTrowelConfirm()
-    {
-        trowelConfirm.SetActive(true);
     }
 }
