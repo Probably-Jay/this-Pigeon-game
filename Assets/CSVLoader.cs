a357b000a81354542aa3fc6d19330874887ea369
Jay <jake.bretherton05@gmail.com>
2021-05-06 15:04:19 +0100

    system in

---
+++ Assets/CSVLoader.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Localisation
+{
+    internal class CSVLoader
+    {
+        
+        public CSVLoader()
+        {
+            Load(); 
+        }
+
+        const string fileName = "localisationData.csv";
+        char lineSeperator = '\n';
+        char[] delimiter = { '#' };
+
+
+        FileStream file;
+        public void Load()
+        {
+            var path = Path.Combine(Application.streamingAssetsPath, fileName);
+            file = File.OpenRead(path);
+        }
+
+        public Dictionary<string, string> GetDictionary(string atributeId)
+        {
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+
+            Load();
+
+            var data = file.ToString();
+
+            string[] lines = data.Split(lineSeperator);
+
+            string[] headers = lines[0].Split(delimiter, System.StringSplitOptions.None);
+
+            int attributeIndex = -1;
+
+            for (int i = 0; i < headers.Length; i++)
+            {
+                if (headers[i].Contains(atributeId))
+                {
+                    attributeIndex = i;
+                    break;
+                }
+            }
+
+            if (attributeIndex == -1)
+            {
+                throw new System.Exception($"This language id {atributeId} is not in this dictaionary");
+            }
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+
+                string[] feilds = line.Split(delimiter, System.StringSplitOptions.None);
+
+
+
+                var key = feilds[0];
+
+                if (dict.ContainsKey(key))
+                {
+                    throw new System.Exception($"This language id {atributeId} attempts to use key {key} in row {i} which is already used previously");
+                }
+
+                if (attributeIndex > feilds.Length)
+                {
+                    throw new System.Exception($"This language id {atributeId} attempts to get a column {attributeIndex} which is not in row {i}");
+                }
+
+                var value = feilds[attributeIndex];
+
+                dict.Add(key, value);
+            }
+
+            return dict;
+
+
+
+        }
+    }
+}
---
+++ Assets/Localiser.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace Localisation
+{
+
+    public enum Language 
+    { 
+        English
+    }
+
+    public enum TextID 
+    { 
+        
+    }
+
+
+    public class Localiser : MonoBehaviour
+    {
+        static Dictionary<Language, Dictionary<TextID, string>> text = null;
+
+        static bool Inited => text != null;
+
+        static Language currentLanguage  = Language.English;
+
+        public static void SetLanguage(Language language)
+        {
+            currentLanguage = language;
+        }
+
+        public static void Init()
+        {
+            CSVLoader loader = new CSVLoader();
+           
+
+            text = new Dictionary<Language, Dictionary<TextID, string>>();
+
+            foreach (var language in Helper.Utility.GetEnumValues<Language>())
+            {
+                LoadLanguage(loader, language);
+            }
+
+        }
+
+        private static void LoadLanguage(CSVLoader loader, Language language)
+        {
+            Dictionary<string, string> rawDictionary = loader.GetDictionary(language.ToString());
+            Dictionary<TextID, string> dictionary = new Dictionary<TextID, string>();
+            foreach (var textID in Helper.Utility.GetEnumValues<TextID>())
+            {
+                if (!rawDictionary.ContainsKey(textID.ToString()))
+                {
+                    throw new System.Exception($"ID {textID} was not present in the data loaded for the language {language}");
+                }
+                dictionary.Add(textID, rawDictionary[textID.ToString()]);
+            }
+
+            text.Add(language, dictionary);
+        }
+
+        public static string GetString(TextID id)
+        {
+            if (!Inited)
+            {
+                Init(); // lazy intitialiseations
+            }
+
+            var value = text[currentLanguage][id];
+
+            return value;
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
